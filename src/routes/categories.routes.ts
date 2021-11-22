f1c739d6809cf9b8b93bec506b9a8c1d03a079c4
João Pedro A <jaaopbr@gmail.com>
2021-11-22 12:16:22 -0300

    ref: refactored routes for better organization

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,25 @@
 import { Router } from "express";
-import { v4 as uuid } from "uuid";
-import { Category } from "../model/category";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import multer from "multer";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  categoriesRepository.create({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const categories = categoriesRepository.list();
+  return listCategoriesController.handle(request, response);
+});
 
-  return response.status(200).json({ data: categories });
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
