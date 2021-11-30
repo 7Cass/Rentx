4b7963d0d6616956b8d05d293c06231de5f00819
João Pedro A <jaaopbr@gmail.com>
2021-11-30 16:35:57 -0300

    🗃️ chore: updated migrations

---
+++ src/database/migrations/1638152653841-CreateCategories.ts
@@ -0,0 +1,24 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCategories1638152653841 implements MigrationInterface {
+    name = 'CreateCategories1638152653841'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "categories",
+                columns: [
+                    { name: "id", type: "uuid", isPrimary: true },
+                    { name: "name", type: "varchar" },
+                    { name: "description", type: "varchar" },
+                    { name: "created_at", type: "timestamp", default: "now()" },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("categories");
+    }
+
+}
