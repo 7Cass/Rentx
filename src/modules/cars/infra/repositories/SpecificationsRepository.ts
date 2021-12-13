26c427c6dffd287c640e25c204e9446712fb4ea2
João Pedro A <jaaopbr@gmail.com>
2021-12-13 19:41:42 -0300

    ♻️ ref: specification create now returns object

--- src/modules/cars/infra/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/repositories/SpecificationsRepository.ts
@@ -9,16 +9,28 @@ class SpecificationsRepository implements ISpecificationsRepository {
     this.repository = getRepository(Specification);
   }
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-    const specification = this.repository.create({name, description, created_at: new Date()});
+  async create({ name, description }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = this.repository.create({
+      name, 
+      description, 
+      created_at: new Date()
+    });
 
-    await this.repository.save(specification);
+    await this.repository.save(specification)
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification | undefined> {
-    const specification = this.repository.findOne({ name });
+    const specification = await this.repository.findOne({ name });
 
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = await this.repository.findByIds(ids);
+
+    return specifications;
   }
 }
 
