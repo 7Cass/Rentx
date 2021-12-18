4421a85e68bb98b22459c414d9b1ed91c5c35169
João Pedro A <jaaopbr@gmail.com>
2021-12-17 22:01:24 -0300

    ✨ feat: added find by id method

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -24,15 +24,23 @@ class RentalsRepository implements IRentalsRepository {
 
         return rental;
     }
+
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
         const openByCar = await this.repository.findOne({ car_id });
 
         return openByCar;
     }
+
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
         const openByUser = await this.repository.findOne({ user_id });
         
         return openByUser;
+    }
+
+    async findById(id: string): Promise<Rental> {
+        const rental = await this.repository.findOne(id);
+
+        return rental;
     }
 };
 
