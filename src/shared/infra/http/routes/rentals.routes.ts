6a25ec2fe00b7f101ec0523f546b08a1a408f286
João Pedro A <jaaopbr@gmail.com>
2021-12-17 22:22:35 -0300

    🚚 feat: adde devolution route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,15 +1,25 @@
-import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { Router } from "express";
+
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.post(
     "/",
     ensureAuthenticated,
     createRentalController.handle
+);
+
+rentalsRoutes.post(
+    "/devolution/:id",
+    ensureAuthenticated,
+    devolutionRentalController.handle
 );
 
 export { rentalsRoutes };
