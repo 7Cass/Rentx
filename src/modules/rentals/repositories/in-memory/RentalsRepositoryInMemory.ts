902f64de8ad36c9284cdad243da3071efa8ca672
João Pedro A <jaaopbr@gmail.com>
2021-12-17 22:01:05 -0300

    ✨ feat: added find by id method

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -24,8 +24,15 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
         return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date);
     }
+
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
         return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date);
+    }
+
+    async findById(id: string): Promise<Rental> {
+        const rental = this.rentals.find(rental => rental.id === id);
+
+        return rental;
     }
 };
 
