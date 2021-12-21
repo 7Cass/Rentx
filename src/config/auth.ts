8d7cce7aa34bf1ae7e0a5e763c79cf8dacf05f21
João Pedro A <jaaopbr@gmail.com>
2021-12-21 02:14:20 -0300

    🔧 chore: added auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "861265cdf8cf8208672ec19c54a79ef8",
+    expires_in_token: "15m",
+    secret_refresh_token: "405499b49cb5027d2bf03b0ac3dbeeff",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
