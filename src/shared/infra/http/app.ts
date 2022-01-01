b1b6aed69f741e932bc040ee28bad171d774b8b2
João Pedro A <jaaopbr@gmail.com>
2022-01-01 06:58:35 -0300

    ✨ feat: added static endpoints

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,7 @@ import "express-async-errors";
 
 import "@shared/container";
 import createConnection from '@shared/infra/typeorm';
+import upload from "@config/upload";
 import { router } from "@shared/infra/http/routes";
 import { AppError } from "@shared/errors/AppError";
 
@@ -17,6 +18,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
